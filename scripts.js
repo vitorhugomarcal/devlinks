536d199077817e5159cf38a7c441f34db2a51ddd
Vitor Hugo Marçal <vitorhugomarcal@gmail.com>
2026-08-29 13:44:31 -0300

    ag3d switch

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light")
+  html.classList.toggle("dark")
 }
 
 function copyPix(event) {
   event.preventDefault();
   const pixCode = "00020126580014BR.GOV.BCB.PIX013625328557-a8f8-42fc-ae2a-e2e2ed8a65dd5204000053039865802BR592566.101.610 TAMMYRIS DIAS 6009SAO PAULO610805409000622505215GeGxBRM1j0ZUFX4nteis6304B1BA";
-  
+
   navigator.clipboard.writeText(pixCode).then(() => {
     const button = document.getElementById("pix-button");
     const originalText = button.textContent;
     button.textContent = "Pix Copiado! ✅";
-    
+
     // Desabilitar cliques temporariamente para evitar cliques duplos durante a animação
     button.style.pointerEvents = "none";
-    
+
     setTimeout(() => {
       button.textContent = originalText;
       button.style.pointerEvents = "auto";
